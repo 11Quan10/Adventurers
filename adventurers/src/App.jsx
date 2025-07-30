d9d70081ef0b0a1d4f7d833b1706cedf8b0ed83b
Quan Pham <qmp210000@utdallas.edu>
2025-07-29 23:29:26 -0500

    first commit + skeleton code

---
+++ adventurers/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { useRoutes, Link } from "react-router-dom";
+import "./App.css";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <div>hello</div>
+        </>
+    );
+}
+
+export default App;
---
+++ adventurers/src/pages/CreateAdventurer.jsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+import { supabase } from "../client";
+
+const CreateAdventurer = () => {};
+
+export default CreateAdventurer;
